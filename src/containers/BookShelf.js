32700cd2cea61a2013a9fce703138a6144146db8
Syed Aman <sna274@nyu.edu>
2017-11-10 13:06:40 -0600

    feat: Create a top-level stateful component that will serve as the home page of the app.
    This will contain the three shelves and books

---
+++ src/containers/BookShelf.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Header from '../components/Header';
+
+/**
+ * A top-level stateful component that serves as the home page of the app.
+ * Displays three shelves with categories "Currently Reading", "Want to Read",
+ * and "Read."
+ * @extends Component
+ */
+class BookShelf extends Component {
+  state = {};
+
+  render() {
+    return <Header />;
+  }
+}
