3fea63e3992216b9e2408de077bce1e5823bad61
Syed Aman <sna274@nyu.edu>
2017-11-10 13:23:22 -0600

    feat: Implement BookShelf as the homepage of the app using react router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import BookShelf from './BookShelf';
+
 import '../assets/styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* BookShelf will serve as the home page. */}
+        <Route path="/" component={BookShelf} exact />
       </div>
     );
   }
