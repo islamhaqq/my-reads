a84b5cc932f00fdef2eed77156f8b7dd99bc6b35
Syed Aman <sna274@nyu.edu>
2017-11-10 12:47:44 -0600

    feat: Create a presentational component that will server as the header in the Book Shelf page
    This shouldn't accept any data of any sort and should just render plain
    old HTML with styles.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Simply displays the title of the app. First thing users look at when they
+ * launch the app.
+ * @method Header
+ */
+const Header = () => (
+  <div>
+    <h1>My Reads</h1>
+  </div>
+);
+
+export default Header;
