1fc79f7a1d632ed507d04b40652eced2daaeb671
Syed Aman <sna274@nyu.edu>
2017-11-10 13:21:04 -0600

    feat: Implement react router to keep track of HTML5 history and allow user navigation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './containers/App';
+
+import './assets/styles/index.css';
 import registerServiceWorker from './lib/services/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // implement React Router for navigation and keeping track of HTML5 history
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
