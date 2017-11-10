6df976aae91c59eda8963c1fb1ddda63dc3bf58a
Syed Aman <sna274@nyu.edu>
2017-11-10 12:41:31 -0600

    docs: Add documentation to Book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/**
+ * Displays a book; its cover, description, and actions.
+ * @method Book
+ * @param  {Object} props - A book object and its data (title, author, etc.)
+ */
 const Book = props => (
   <div>
     {/* Book cover photo. */}
