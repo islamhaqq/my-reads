3b24729fa94327ac435b2d21792568f07d3194a4
Syed Aman <sna274@nyu.edu>
2017-11-14 13:58:49 -0600

    feat: Dynamically render out all the books in a bookshelf
    Use Array.prototype.map instead of hard coding the books in.

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -17,20 +17,16 @@ const BookShelf = props => (
     {/* Listed out books. */}
     <div className="bookshelf-books">
       <ol className="books-grid">
-        <li>
-          <Book
-            coverImageSource="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
-            title="To Kill a Mockingbird"
-            author="Harper Lee"
-          />
-        </li>
-        <li>
-          <Book
-            coverImageSource="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-            title="Currently Reading"
-            author="Orson Scott Card"
-          />
-        </li>
+        {props.books.map(book => (
+          <li>
+            <Book
+              coverImageSource={book.coverImageSource}
+              title={book.title}
+              author={book.authors[0]}
+              key={book.title}
+            />
+          </li>
+        ))}
       </ol>
     </div>
   </div>
