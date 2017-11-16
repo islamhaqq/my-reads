befdeba8eb200be6cc892ab5a34563375a8f85c4
Syed Aman <sna274@nyu.edu>
2017-11-16 15:37:26 -0600

    fix: Fix error when authors or imagelinks weren't provided by API by using validation

--- src/components/BooksGrid.js
+++ src/components/BooksGrid.js
@@ -3,25 +3,31 @@ import PropTypes from 'prop-types';
 
 import Book from './Book';
 
-const BooksGrid = props => (
-  <div className="search-books-results">
-    <ol className="books-grid">
-      {props.books && props.books.length > 0 ? (
-        props.books.map(book => (
-          <li key={book.id}>
-            <Book
-              coverImageSource={book.imageLinks.thumbnail}
-              title={book.title}
-              author={book.authors[0]}
-            />
-          </li>
-        ))
-      ) : (
-        <h1>No Results...</h1>
-      )}
-    </ol>
-  </div>
-);
+const BooksGrid = props => {
+  console.log(props.books);
+
+  return (
+    <div className="search-books-results">
+      <ol className="books-grid">
+        {props.books && props.books.length > 0 ? (
+          props.books.map(book => (
+            <li key={book.id}>
+              <Book
+                coverImageSource={
+                  book.imageLinks ? book.imageLinks.thumbnail : ''
+                }
+                title={book.title}
+                author={book.authors ? book.authors[0] : ''}
+              />
+            </li>
+          ))
+        ) : (
+          <h1>No Results...</h1>
+        )}
+      </ol>
+    </div>
+  );
+};
 
 BooksGrid.propTypes = {
   /**
